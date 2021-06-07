9a85f2791f3edd83ee37bc1df07c936d3abd67c9
Leonardo Marcos <marcosleonardosss@gmail.com>
2021-06-06 22:50:15 -0300

    create posts endpoint

---
+++ src/pages/api/posts/data.ts
@@ -0,0 +1,82 @@
+export default {
+  result: [
+    {
+      title: 'Atenção!',
+      subtitle: 'Estamos mudando de sala!',
+      description: 'Continuaremos no campus Porto Alegre do IFRS, voltaremos em breve.',
+      createdAt: '2021-03-05T23:27:16-03:00',
+      updatedAt: '2021-03-05T23:27:16-03:00',
+    },
+    {
+      title: 'Open Day',
+      subtitle: 'Nas sextas-feiras das 14h às 18h!',
+      description: 'O poaLAB estará aberto para visitação da comunidade externa.',
+      slug: 'open-day',
+      createdAt: '2021-03-05T23:27:16-03:00',
+      updatedAt: '2021-03-05T23:27:16-03:00',
+      article: {
+        image: 'http://www.poa.ifrs.edu.br/images/Fotos/maker_intel-003.jpg',
+        title:
+          'Portas abertas para colocar ideias em prática: conheça o PoaLab, laboratório de fabricação digital do Campus Porto Alegre',
+        text: `Proporcionar estímulo para o empreendedorismo local, bem como um lugar para jogar, criar, aprender, orientar e inventar: são apenas algumas das possibilidades oferecidas pelo PoaLab, laboratório de fabricação digital - ou Fab Lab - sediado como um programa de extensão no IFRS - Campus Porto Alegre.
+
+Fab Lab é uma abreviação para “laboratório de fabricação” em inglês – um espaço em que pessoas de diversas áreas se reúnem para realizar projetos de fabricação digital de forma colaborativa. Ser um Fab Lab significa estar conectado a uma comunidade mundial de alunos, educadores, técnicos, pesquisadores e inovadores e desenvolver atividades dentro da filosofia do conhecimento aberto.
+
+A rede Fab Lab caracteriza-se como uma plataforma de prototipagem rápida para a aprendizagem, inovação e invenção utilizando fabricação digital. Funcionando desde 2015 no IFRS – Campus Porto Alegre, o PoaLab é um Fab Lab conectado à rede mundial de Fab Labs iniciada no Massachusetts Institute of Technology (MIT), um dos centros de ciência e tecnologia mais importantes do mundo, localizado em Massachusetts, nos Estados Unidos.
+
+O professor do Campus Porto Alegre e coordenador do PoaLab, André Peres, explica que “os Fab Labs são uma iniciativa do Centro de Bits e Átomos do MIT que traz para a comunidade acesso às ferramentas de fabricação digital (impressoras 3D, máquinas de corte à laser, máquinas de corte de vinil, fresadoras de precisão e de grande porte) e de prototipação eletrônica (como os controladores Arduíno) em um ambiente colaborativo e criativo. A rede de Fab Labs possui aproximadamente mil laboratórios no mundo que contam com um conjunto padronizado de equipamentos, permitindo que um projeto desenvolvido em qualquer um dos laboratórios possa ser replicado nos demais.” 
+
+Conforme Peres, as possibilidades de um Fab Lab são realmente ilimitadas. “Em um Fab Lab o usuário tem a possibilidade de transformar ideias em objetos. Qualquer coisa dentro dos limites de capacidade dos equipamentos e que possa ser representado em bits (basicamente desenhos 2D e 3D no computador) pode ser transformado em átomos no PoaLab”. 
+
+Em um Fab Lab é possível construir "quase qualquer coisa". “Temos a capacidade de modelar e imprimir peças plásticas de até 15x15x15cm em impressoras 3D, corte à laser em peças de até 60x40cm em madeira, acrílico e couro, corte de peças de 80cm por infinito em vinil e papel na plotter de recorte, prototipação eletrônica e a fresadora de precisão para criação de peças em madeira, acrílico e criação de placas de circuitos eletrônicos”, exemplifica o professor.
+
+Os projetos com maior visibilidade na rede Fab Lab são os de criação de próteses, mobiliário, robôs, objetos para ensino de ciências, objetos para performances artisticas e desenvolvimento de máquinas do próprio laboratório como impressoras 3D, fresadoras etc. 
+
+Além de estar na rede Fab Lab mundial, o PoaLab faz parte da rede [Fab Lab Brasil](http://redefablabbrasil.org/). Para entrar na rede, os Labs precisam se inscrever no site <https://www.fablabs.io> e aguardar aprovação da [Fab Foundation](http://www.fabfoundation.org/).
+
+Hoje, no Brasil, são 40 Fab Labs certificados e atuando com os princípios propostos pela iniciativa (Fonte: <https://www.fablabs.io/labs?country=br>. Acesso em 25 de agosto de 2017). Entre os Fab Labs brasileiros, o PoaLab ainda é o único pertencente a uma instituição da rede federal de educação profissional e tecnológica: o Campus Porto Alegre é o primeiro campus de um Instituto Federal de Educação, Ciência e Tecnologia a possuir o espaço.
+
+#### Sobre o PoaLab 
+
+Desde 2015 cadastrado na rede mundial de Fab Labs, em 2016 o laboratório iniciou suas atividades como um programa de extensão do IFRS – Campus Porto Alegre.
+
+![imagem equipamentos](http://www.poa.ifrs.edu.br/images/Fotos/poalab2.JPG)
+
+O PoaLab hoje é um local de compartilhamento não só de espaço físico e equipamentos, mas de ideias; nele é possível, por exemplo, materializar estas ideias e prototipar produtos com baixo custo e curto espaço de tempo. Para isso, o espaço conta com equipamentos como impressora 3D, cortadora laser/máquina de corte, fresadora de precisão, plotter de recorte e plotter de impressão, extrusora de filamento, além de uma bancada de eletrônica com osciloscópio, fonte de bancada, estação de solda e componentes eletrônicos diversos.
+
+Segundo Peres, a proposta de um laboratório de fabricação digital no IFRS surgiu ainda em 2012. Ele explica que o PoaLab tornou-se viável a partir de diversos projetos de pesquisa envolvendo tecnologia, internet das coisas e educação.
+
+“A ideia de termos um espaço de criação utilizando fabricação digital surgiu em 2012 quando começamos a trabalhar em projetos de pesquisa envolvendo a internet das coisas. Verificou-se a necessidade de termos maior capacidade na criação de protótipos e a disponibilização deste espaço para a comunidade interna e externa ao IFRS.”
+
+Isso foi possível graças à cooperação entre professores da área de informática do IFRS campus Porto Alegre que disponibilizaram os equipamentos obtidos em projetos de pesquisa finalizados entre 2012 e 2015. Estes equipamentos foram agrupados durante este período e, após o término dos projetos, foram instalados na sala onde hoje é o PoaLab.
+
+Peres lembra que, além dele, o qual atua na coordenação do laboratório, há outros agentes envolvidos que colaboram com a operacionalização do PoaLab, como os professores do IFRS Evandro Miletto, Fábio Okuyama e Karen Borges. Além disso, “temos alunos voluntários em diversos projetos de extensão e a comunidade de usuários (tanto interna ao IFRS quanto externa) desempenha papel fundamental na construção do espaço como ambiente colaborativo e inovador”, complementa.
+
+#### Aberto à comunidade
+
+Além de estar à disposição dos alunos da instituição para atividades de aula e projetos acadêmicos, o espaço tem como característica abrir suas portas para a comunidade, permitindo que ideias sejam colocadas em prática: são os chamados Open days (dias abertos, em livre tradução).
+
+![imagem porta laboratorio](http://www.poa.ifrs.edu.br/images/Fotos/maker_intel-001.jpg)
+
+Os Open days são um dos requisitos para a existência de um Fab Lab. Nestes dias, a comunidade externa tem a possibilidade de conhecer as ferramentas existentes no lab e aprender sobre fabricação digital.
+
+“Como o PoaLab é um programa de extensão, ele tem a capacidade de atender tanto o público interno quanto o externo. Oferecemos oficinas e eventos abertos e gratuitos, além do open day, dia da semana no qual qualquer pessoa pode vir ao lab para conhecer os equipamentos e começar a prototipar”, explica Peres.
+
+Os Open days no PoaLab acontecem todas as quartas-feiras, no horário das 14h às 18h. Neste segundo semestre de 2017, as atividades terão início no dia 06 de setembro.
+
+O PoaLab localiza-se na sala 1016, 10º andar da torre Sul do IFRS – Campus Porto Alegre (rua Coronel Vicente, 281, Centro Histórico, Porto Alegre - RS). O contato pode ser feito pelo e-mail andre.peres@poa.ifrs.edu.br ou por meio da página do PoaLab no [Facebook](facebook.com/poalab).
+
+#### Projetos em andamento e futuras parcerias
+
+De acordo com o coordenador do PoaLab, atualmente estão se desenvolvendo projetos iniciais que servirão como base para o estabelecimento de projetos com parceiros. Um dos projetos envolve o desenvolvimento de equipamentos para monitoramento ambiental (qualidade da água e do ar); outro o desenvolvimento de impressoras 3D de baixo custo; um terceiro projeto de criação de placas braille utilizando impressora 3D e ainda um projeto para criação de objetos impressos em 3D para ensino de morfologia celular para deficientes visuais. 
+
+Sobre as perspectivas futuras do espaço no campus Porto Alegre, Peres menciona que estão previstas algumas ações de curto prazo, tais como a ampliação do horário de operação do laboratório, contratação de bolsistas para atendimento aos usuários (chamados “gurus”), ampliação da inserção do lab em escolas públicas e ampliação de conexão com as comunidades locais para identificação de projetos inovadores. Em médio e longo prazos, pretende-se consolidar parcerias que possibilitem a manutenção destes projetos e permitam a ampliação dos serviços do PoaLab.
+
+“É importante que as pessoas percebam que a fabricação digital estará presente na vida de todos da mesma forma que a internet. A fabricação pessoal será uma realidade assim como a apropriação da responsabilidade pela construção de soluções para os nossos problemas. Da mesma forma que os laboratórios de informática desempenharam um papel fundamental na década de 80/90 para o desenvolvimento da computação, os Fab Labs servirão para a capacitação de uma geração de makers que serão os protagonistas desta nova realidade”, finaliza o professor.
+
+![imagem pessoas no poalab](http://www.poa.ifrs.edu.br/images/Fotos/maker_intel-006.jpg)
+          `,
+      },
+    },
+  ],
+};
