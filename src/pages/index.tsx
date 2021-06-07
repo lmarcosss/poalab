b8a41bbc69564af7fdbda55c33d99b502a59638e
Leonardo Marcos <marcosleonardosss@gmail.com>
2021-06-06 22:52:49 -0300

    create post page and adjust home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
+import { GetStaticProps } from 'next';
 import { Box, Text } from '@chakra-ui/react';
+
 import { Swiper } from '../components/Swiper';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
+import { Banner } from '../components/Banner';
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <Box>
       <Header />
-      <Swiper />
+      <Swiper Component={Banner} items={posts} />
 
       <Text
         fontWeight="medium"
@@ -58,3 +61,15 @@ export default function Home() {
     </Box>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch(`${process.env.API_ENDPOINT}/api/posts`);
+  const data = await response.json();
+
+  return {
+    props: {
+      posts: data.result,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
