0cdf23cbff60dc31fe82ec91b7f9c0daec51ff94
Francisco Wolfarth Mossi <francisco.mossi@cwi.com.br>
2021-07-21 21:55:10 -0300

    adding link component and api data

---
+++ src/pages/api/links/data.ts
@@ -0,0 +1,75 @@
+export default {
+  result: [
+    {
+      description: 'Impressão 3D de Placas Braille Site do CTA/IFRS',
+      url: 'https://cta-ifrs.github.io/Text2Braille3d/',
+      title: 'Braille',
+      image: 'https://www.poalab.net.br/images/recursos/braille.png',
+    },
+    {
+      url: 'https://www.thingiverse.com/',
+      title: 'Thingiverse',
+      image: 'https://www.poalab.net.br/images/recursos/thingiverse.png',
+    },
+    {
+      url: 'https://www.instructables.com/',
+      title: 'instructables',
+      image: 'https://www.poalab.net.br/images/recursos/InstructablesRobot.png',
+    },
+    {
+      url: 'https://www.opendesk.cc/',
+      title: 'Opendesk',
+      image: 'https://www.poalab.net.br/images/recursos/opendesk.png',
+    },
+    {
+      url: 'http://fabacademy.org/',
+      title: 'Fab Academy',
+      image: 'https://www.poalab.net.br/images/recursos/fabacademy.png',
+    },
+    {
+      url: 'http://www.fabfoundation.org/',
+      title: 'Fab Foundation',
+      image: 'https://www.poalab.net.br/images/recursos/fabfoundation.png',
+    },
+    {
+      url: 'https://fab.city/',
+      title: 'Fab City',
+      image: 'https://www.poalab.net.br/images/recursos/fabcity.png',
+    },
+    {
+      url: 'https://makezine.com/',
+      title: 'makezine',
+      image: 'https://www.poalab.net.br/images/recursos/make.png',
+    },
+    {
+      url: 'https://www.tinkercad.com/',
+      title: 'tinkercad',
+      image: 'https://www.poalab.net.br/images/recursos/tinkercad.png',
+    },
+    {
+      url: 'https://inkscape.org/pt-br/',
+      title: 'inkscape',
+      image: 'https://www.poalab.net.br/images/recursos/inkscape.png',
+    },
+    {
+      url: 'http://www.makercase.com/',
+      title: 'makercase',
+      image: 'https://www.poalab.net.br/images/recursos/makercase.png',
+    },
+    {
+      url: 'http://aprendizagemcriativa.org/',
+      title: 'RBAC',
+      image: 'https://www.poalab.net.br/images/recursos/rbac.png',
+    },
+    {
+      url: 'https://portoalegre.makerfaire.com/',
+      title: 'maker faire porto alegre',
+      image: 'https://www.poalab.net.br/images/recursos/makepoa.png',
+    },
+    {
+      url: 'https://fabbernatives/',
+      title: 'Braille',
+      image: 'https://www.poalab.net.br/fabbernatives/images/logo.svg',
+    },
+  ],
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps } from 'next';
 import {
   Box,
   Text,
+  Flex,
   SimpleGrid,
 } from '@chakra-ui/react';
 
@@ -11,9 +12,10 @@ import {
   Footer,
   Banner,
   Equipment,
+  Link,
 } from '../components';
 
-export default function Home({ posts, equipments }) {
+export default function Home({ posts, equipments, links }) {
   return (
     <Box>
       <Header />
@@ -59,17 +61,26 @@ export default function Home({ posts, equipments }) {
           ))}
         </SimpleGrid>
       </Box>
-      <Text
-        fontWeight="medium"
-        h="512"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        id="links"
-        name="links"
-      >
-        Links úteis
-      </Text>
+      {/* <Box>
+        <Text
+          fontWeight="medium"
+          h="512"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          id="links"
+          name="links"
+        >
+          Links úteis
+        </Text>
+        <SimpleGrid columns={{ lg: 4, sm: 2, md: 3}} spacing={10} padding={20}>
+          {links.map((link, index) => (
+            <Flex justify="center">
+              <Link {...link} key={index} />
+            </Flex>
+          ))}
+        </SimpleGrid>
+      </Box> */}
 
       <Footer />
     </Box>
@@ -91,14 +102,20 @@ const getEquipments = async () => {
   return await fetchData('https://poalab.vercel.app/api/equipments');
 }
 
+const getLinks = async () => {
+  return await fetchData('https://poalab.vercel.app/api/links')
+}
+
 export const getStaticProps: GetStaticProps = async () => {
   const posts = await getPosts();
   const equipments = await getEquipments();
+  // const links = await getLinks();
 
   return {
     props: {
       posts,
       equipments,
+      // links,
     },
     revalidate: 60 * 60 * 24, // 24 hours
   };
